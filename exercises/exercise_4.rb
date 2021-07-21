d998a4198e817d3304be2152e4da1e496a90e79d
Jacob <jacob.fitzgerald@outlook.com>
2021-07-21 21:35:17 +0000

    add ex4 add 3 new stores and make a where request to find all mens cloathing

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,30 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+class Store
+
+  store = Store.new
+  store.id
+  store.name = "Surrey"
+  store.annual_revenue = "224000"
+  store.mens_apparel = false
+  store.womens_apparel = true
+  store.save
+
+  store = Store.new
+  store.name = "Whistler"
+  store.annual_revenue = "1900000"
+  store.mens_apparel = true
+  store.womens_apparel = false
+  store.save
+
+  store = Store.new
+  store.name = "Yaletown"
+  store.annual_revenue = "430000"
+  store.mens_apparel = true
+  store.womens_apparel = true
+  store.save
+
+  puts store = Store.where(mens_apparel: true)
+
+end
